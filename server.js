dd00e16e6854a4dc7f152b7b313eb4d9965058e3
Abdessamed <ka_seddiki@esi.dz>
2023-07-03 00:55:57 +0100

    authors

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,12 +15,14 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-app.use('/', indexRouter)
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
 useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
