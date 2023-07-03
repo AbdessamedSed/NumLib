96aafe4993b204cadd213ed78b5c2b3e020b3861
Abdessamed <ka_seddiki@esi.dz>
2023-07-04 00:06:14 +0100

    auth pp

--- routes/authors.js
+++ routes/authors.js
@@ -2,16 +2,40 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
 
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors : authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 router.get('/new', (req, res) => {
     res.render('authors/new', { author : new Author() })
 })
 
-router.post('/', (req, res) => {
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.surname
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+
+    } catch {
+        res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error'
+        })
+    }
 })
 
 module.exports = router
